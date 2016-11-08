a283124d4ea59774e67afcbf7df56c447099382d
matsuda-koushi <cauchymazda@r.recruit.co.jp>
2016-11-08 09:53:37 +0900

    eslint設定した

--- carousel/webpack.config.js
+++ carousel/webpack.config.js
@@ -5,9 +5,19 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ],
     loaders: [
       { test: /\.css$/, loader: "style!css" }
     ]
+  },
+  eslint: {
+    configFile: './.eslintrc'
   }
 };
 
